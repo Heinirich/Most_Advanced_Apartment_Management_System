52453fe98441b73a85b50930b446381888a47365
Heinrich <smithheinrich254@gmail.com>
2022-12-27 00:07:49 +0300

    Database Backup

---
+++ app/Console/Commands/DatabaseBackUp.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+
+class DatabaseBackUp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Database Backup';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
+  
+        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
+  
+        $returnVar = NULL;
+        $output  = NULL;
+  
+        exec($command, $output, $returnVar);
+        return Command::SUCCESS;
+    }
+}
